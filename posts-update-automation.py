26894b5bdbbdcf90f938b194c64e5ab5a2973e49
Nam Gi Beom <rlqja0523@naver.com>
2024-01-09 23:36:15 +0900

    Update script file

--- posts-update-automation.py
+++ posts-update-automation.py
@@ -1,35 +1,23 @@
 import feedparser
 import os
 from markdownify import markdownify
-from pygments.lexers import guess_lexer
 
 
-def post(feeds: list):
+def update(feeds: list):
     for feed in feeds:
         category = feed["tags"][0]["term"]
         title = feed["title"]
         content = create_content(title, feed["summary"])
 
         file_name = get_file_name(category, title)
-        f = open(file_name, mode="w", encoding="utf-8")
-        f.write(content)
-        f.close()
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.write(content)
+        update_readme(category)
 
 
 def create_content(title: str, summary: str) -> str:
     markdown_summary = markdownify(summary)
-    return attach_language_name(f"{title}\n=\n{markdown_summary}")
-
-
-def attach_language_name(content: str) -> str:
-    code_block = "```"
-    blocks = content.split(code_block)
-
-    for i in range(1, len(blocks), 2):
-        code = blocks[i]
-        lexer = guess_lexer(code)
-        blocks[i] = code_block + " " + lexer.name + code + code_block + "\n"
-    return "".join(blocks)
+    return f"{title}\n=\n{markdown_summary}"
 
 
 def get_file_name(category: str, title: str) -> str:
@@ -38,7 +26,18 @@ def get_file_name(category: str, title: str) -> str:
     return file_path + "README.md"
 
 
+def update_readme(category: str):
+    with open("README.md", "r", encoding="utf-8") as f:
+        readme = f.read()
+
+    if readme.find(category) == -1:
+        with open("README.md", "a", encoding="utf-8") as f:
+            f.write(
+                f"- [{category}](https://github.com/GIVEN53/blog-post/tree/main/{category})\n"
+            )
+
+
 if __name__ == "__main__":
     tistory_blog_uri = "https://given-dev.tistory.com"
     feeds = feedparser.parse(tistory_blog_uri + "/rss")
-    post(feeds["entries"])
+    update(feeds["entries"])
