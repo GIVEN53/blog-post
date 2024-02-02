79982f1fb181b2ec791cadc4e699c20359ebab3e
Nam Gi Beom <rlqja0523@naver.com>
2024-02-02 21:29:44 +0900

    Fix toc sorting in README.md

--- posts-update-automation.py
+++ posts-update-automation.py
@@ -51,7 +51,22 @@ def update_readme(category: str):
 
     if readme.find(category) == -1:
         with open("README.md", "a", encoding="utf-8") as f:
-            f.write(f"- [{category}]({GITHUB_URI + category})\n")
+            f.write(f"\n- [{category}]({GITHUB_URI + category})")
+
+    sort_toc()
+
+
+def sort_toc():
+    with open("README.md", "r", encoding="utf-8") as f:
+        readme = f.read()
+
+    start = readme.find("## 목차")
+    toc = readme[start:].strip()
+    toc_lines = sorted(toc.split("\n")[1:])
+    sort_toc = "\n".join(["## 목차"] + toc_lines)
+
+    with open("README.md", "w", encoding="utf-8") as f:
+        f.write(readme.replace(toc, sort_toc))
 
 
 if __name__ == "__main__":
